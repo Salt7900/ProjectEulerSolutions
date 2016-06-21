01c2bc131e0b472b9700b81f3b4fb1033bdbe46c
Ben Fallon <bnfallon@gmail.com>
2016-06-21 15:17:26 -0500

    create class

--- PE10_Primes_Sums/CPP/CPP_PE10_Primes_Sums.cpp
+++ PE10_Primes_Sums/CPP/CPP_PE10_Primes_Sums.cpp
@@ -4,6 +4,20 @@
 #include <time.h>
 using namespace std;
 
+class Sieve{
+public:
+  Sieve(unsigned maxval=4000000);
+  long long sum(unsigned val = 0) const;
+  unsigned primesBelow(unsigned val) const;
+  bool isPrime(unsigned val) const;
+private:
+  bool clr(unsigned val) { nums[val>>1] = false; }
+  bool get(unsigned val) const { return nums[val>>1]; }
+  unsigned adjust(unsigned val) const {return (val ==0 || val> maxnum) ? maxnum : val; }
+  unsigned maxnum;
+  std::vector<bool>nums;
+};
+
 bool is_prime(int number_to_check, std::vector<int> primes){
   if(number_to_check == 2){
     return true;
@@ -24,18 +38,22 @@ int main(){
   int f;
   t = clock();
 
-  int prime_sum = 0;
-  int number_check = 2;
-  std::vector<int> primes;
-  while(number_check<2000000){
-    if (is_prime(number_check, primes)){
-      prime_sum += number_check;
-      primes.push_back(number_check);
-      cout << number_check << endl;
-    };
-    ++number_check;
-  };
-  cout << prime_sum << endl;
+  // int prime_sum = 0;
+  // int number_check = 2;
+  // std::vector<int> primes;
+  // while(number_check<2000000){
+  //   if (is_prime(number_check, primes)){
+  //     prime_sum += number_check;
+  //     primes.push_back(number_check);
+  //     cout << number_check << endl;
+  //   };
+  //   ++number_check;
+  // };
+  // cout << prime_sum << endl;
+
+  const unsigned maxprime = 2000000;
+  Sieve sv(maxprime);
+  std::cout << sv.sum
 
   t = clock() - t;
   printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
