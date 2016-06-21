a194617b8eda0df079980c1972e13ffe6bbf1d1b
Ben Fallon <bnfallon@gmail.com>
2016-06-21 15:05:11 -0500

    drop speed from ~3317s to ~1490s

--- PE10_Primes_Sums/CPP/CPP_PE10_Primes_Sums.cpp
+++ PE10_Primes_Sums/CPP/CPP_PE10_Primes_Sums.cpp
@@ -4,22 +4,19 @@
 #include <time.h>
 using namespace std;
 
-bool is_prime(int number_to_check){
+bool is_prime(int number_to_check, std::vector<int> primes){
   if(number_to_check == 2){
     return true;
-  }else if(number_to_check > 2 && number_to_check%2 != 0){
-    int divisor_counter = 0;
-    for(int counter = 2; counter<number_to_check; ++counter){
-      if (number_to_check%counter == 0){
-        ++divisor_counter;
+  }else if(number_to_check%2 != 0){
+    for(int counter=0; counter<(primes.size()); counter++){
+      if(number_to_check%(primes[counter]) == 0){
+        return false;
       };
     };
-    if(divisor_counter>0){
-      return false;
-    }else{
-      return true;
-    };
-  }else{return false;}
+    return true;
+  }else{
+    return false;
+  };
 };
 
 int main(){
@@ -29,9 +26,11 @@ int main(){
 
   int prime_sum = 0;
   int number_check = 2;
+  std::vector<int> primes;
   while(number_check<2000000){
-    if (is_prime(number_check)){
+    if (is_prime(number_check, primes)){
       prime_sum += number_check;
+      primes.push_back(number_check);
       cout << number_check << endl;
     };
     ++number_check;
