db7155e17e6a937275bd08e33f4a1d71eb6cac1e
Ben Fallon <bnfallon@gmail.com>
2016-06-21 10:57:55 -0500

    reduce runtime from ~18 seconds to ~8 seconds

--- PE7_10001_Prime/CPP/CPP_PE7_10001_Prime.cpp
+++ PE7_10001_Prime/CPP/CPP_PE7_10001_Prime.cpp
@@ -5,17 +5,21 @@
 using namespace std;
 
 bool is_prime(int number_to_check){
-  int divisor_counter = 0;
-  for(int counter = 2; counter<number_to_check; ++counter){
-    if (number_to_check%counter == 0){
-      ++divisor_counter;
-    };
-  };
-  if(divisor_counter>0){
-    return false;
-  }else{
+  if(number_to_check == 2){
     return true;
-  };
+  }else if(number_to_check > 2 && number_to_check%2 != 0){
+    int divisor_counter = 0;
+    for(int counter = 2; counter<number_to_check; ++counter){
+      if (number_to_check%counter == 0){
+        ++divisor_counter;
+      };
+    };
+    if(divisor_counter>0){
+      return false;
+    }else{
+      return true;
+    };
+  }else{return false;}
 };
 
 int main(){
